06cae79dea6cba8e2727c40051d09657dcedb644
lcanas <luca.canas@urbetrack.com>
2024-10-27 23:19:38 -0300

    Init project

---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <header className="bg-teal-400 py-5">
+            <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumos</h1>
+        </header>
+    );
+}
+
+export default Header;
